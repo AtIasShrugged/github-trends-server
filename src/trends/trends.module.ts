45e5736135f52a0e9131dad0a27042a41207bea9
AtIasShrugged <btw210899@gmail.com>
2022-02-05 19:42:10 +0200

    Added HttpModule (Axios) into TrendsModule

--- src/trends/trends.module.ts
+++ src/trends/trends.module.ts
@@ -1,8 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { TimedRequestModule } from 'src/timed-request/timed-request.module';
 import { TrendsController } from './trends.controller';
 import { TrendsService } from './trends.service';
 
 @Module({
+  imports: [TimedRequestModule, HttpModule],
   controllers: [TrendsController],
   providers: [TrendsService],
 })
