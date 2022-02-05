04db9084eb81306cea80f05bf3279d1b85e6d653
AtIasShrugged <btw210899@gmail.com>
2022-02-05 19:40:58 +0200

    Moved sync API back to trends controller
    Remove timed-request controller
    Implemented a timed-request service in the trends controller

--- src/timed-request/timed-request.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('timed-request')
-export class TimedRequestController {
-  @Get('sync')
-  sync() {
-    return 'This action reset the internal timer for the automatic sync';
-  }
-}
--- src/trends/trends.controller.ts
+++ src/trends/trends.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { TimedRequestService } from 'src/timed-request/timed-request.service';
 import { SearchQueryDto } from './dto/search-query.dto';
 import { TrendsService } from './trends.service';
+import { Logger } from '@nestjs/common';
 
 @Controller('trends')
 export class TrendsController {
-  constructor(private readonly trendsService: TrendsService) {}
+  constructor(
+    private readonly trendsService: TrendsService,
+    private readonly timedRequestService: TimedRequestService,
+  ) {
+    this.timedRequestService.init((mes) => Logger.log(mes), 5000, 'test');
+  }
 
   @Get()
   findAll(): string {
     return this.trendsService.findAll();
+  }
+
+  @Get('sync')
+  sync() {
+    return this.timedRequestService.sync();
   }
 
   @Get('repo/:id')
