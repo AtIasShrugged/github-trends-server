bcd5755fe868df9c6f9fa63fb40647fd62f1e74e
Atlas <btw210899@gmail.com>
2022-02-05 13:42:11 +0200

    Added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TrendsModule } from './trends/trends.module';
 import { TimedRequestModule } from './timed-request/timed-request.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [TrendsModule, TimedRequestModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    TrendsModule,
+    TimedRequestModule,
+  ],
   controllers: [],
   providers: [],
 })
