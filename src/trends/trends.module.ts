313ddd3b3a19deb234821ecebbf3d1d21e642668
AtIasShrugged <btw210899@gmail.com>
2022-02-05 21:53:03 +0200

    Changed Nest httpModule to Axios

--- src/trends/trends.module.ts
+++ src/trends/trends.module.ts
@@ -1,11 +1,20 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TimedRequestModule } from 'src/timed-request/timed-request.module';
+import {
+  TrendRepository,
+  TrendRepositorySchema,
+} from './schemas/trendRepository.schema';
 import { TrendsController } from './trends.controller';
 import { TrendsService } from './trends.service';
 
 @Module({
-  imports: [TimedRequestModule, HttpModule],
+  imports: [
+    TimedRequestModule,
+    MongooseModule.forFeature([
+      { name: TrendRepository.name, schema: TrendRepositorySchema },
+    ]),
+  ],
   controllers: [TrendsController],
   providers: [TrendsService],
 })
