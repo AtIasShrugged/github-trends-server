a48cb45e230667a4b7a34494153c3f47398319cb
AtIasShrugged <btw210899@gmail.com>
2022-02-05 23:02:21 +0200

    Added create-trend.dto

---
+++ src/trends/dto/create-trend.dto.ts
@@ -0,0 +1,15 @@
+export class CreateTrendDto {
+  repositoryName: string;
+
+  username: string;
+
+  url: string;
+
+  description: string;
+
+  language: string;
+
+  totalStars: number;
+
+  forks: number;
+}
