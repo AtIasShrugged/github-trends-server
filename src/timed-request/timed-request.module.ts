386a82590e7c581be39b448b2da09ce5fd66bc9e
Atlas <btw210899@gmail.com>
2022-02-05 22:19:29 +0200

    Removed timed-request controller from module

--- src/timed-request/timed-request.module.ts
+++ src/timed-request/timed-request.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TimedRequestService } from './timed-request.service';
-import { TimedRequestController } from './timed-request.controller';
 
 @Module({
   providers: [TimedRequestService],
-  controllers: [TimedRequestController],
   exports: [TimedRequestService],
 })
 export class TimedRequestModule {}
